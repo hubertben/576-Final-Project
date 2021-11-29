8c6e6f45ca6e43b4497b010a9491293176cb096b
rsalz47 <ronan.salz@gmail.com>
2021-11-29 01:07:24 -0500

    movement and inventory interaction
    Developed starting room a little bit. Added a sample inventory spot, where text pops up when you walk up to it, and you can open a rudimentary menu.

---
+++ Assets/Scripts/open_inventory_menu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class open_inventory_menu : MonoBehaviour
+{
+    public GameObject inventoryMenu;
+    public GameObject interactionInstructions;
+    public GameObject inventory;
+    public GameObject inventorySpot;
+
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if player is inside a square surrounding the inventory object, tell them how to open inventory
+        if(transform.position.x < inventorySpot.transform.position.x + 4 && transform.position.x > inventorySpot.transform.position.x - 4 &&
+           transform.position.z < inventorySpot.transform.position.z + 4 && transform.position.z > inventorySpot.transform.position.z - 4)
+        {
+            inventoryMenu.SetActive(true);
+            //if they press E, open the inventory
+            if(Input.GetKey(KeyCode.E))
+            {
+                interactionInstructions.SetActive(false);
+                inventory.SetActive(true);
+            }
+            else
+            {
+                interactionInstructions.SetActive(true);
+                inventory.SetActive(false);
+            }
+        }
+        else
+        {
+            inventoryMenu.SetActive(false);
+        }
+    }
+}
