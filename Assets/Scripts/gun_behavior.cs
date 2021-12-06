4f2100e3b0915bfe1759819e1bd238abb686f332
rsalz47 <ronan.salz@gmail.com>
2021-12-06 11:52:50 -0500

    minor weapon adjustments

--- Assets/Scripts/gun_behavior.cs
+++ Assets/Scripts/gun_behavior.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class gun_motion : MonoBehaviour
+public class gun_behavior : MonoBehaviour
 {
     private Animator animcon;
     public Animator playerAnimCon;
+    public GameObject projectile;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +20,8 @@ public class gun_motion : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && animcon.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
         {
             animcon.SetTrigger("ShootingProjectile");
+            Vector3 proj_dir = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+            Instantiate(projectile, proj_dir, transform.rotation);
         }
     }
 }
