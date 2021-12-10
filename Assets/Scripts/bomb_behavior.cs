d4a45b5ad6aa1d32cad942e9d6d3ad588815f6cb
Michael Chang <michael@MichaeldeMacBook-Pro.local>
2021-12-10 12:53:43 -0500

    fix pull

--- Assets/Scripts/bomb_behavior.cs
+++ Assets/Scripts/bomb_behavior.cs
@@ -69,8 +69,7 @@ public class bomb_behavior : MonoBehaviour
         
         if(collision.gameObject.name.Contains("Boss"))
         {
-
-            //decrement boss health by dmg
+             //decrement boss health
         }
         //MasterBomb special attack
         if(gameObject.name.Contains("Master") && !gameObject.name.Contains("Split"))
